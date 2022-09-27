876d4750d5d6e22826945c7771e50456d7e3ac5a
Klaus Kupper <klausdk1999@gmail.com>
2022-09-27 14:49:47 -0300

    feat: prisma schema

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import cors from "cors";
+import express from "express";
+import "express-async-errors";
+import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
+import agendaRouter from "./routers/agendaRouter.js";
+import e2eRouter from "./routers/testRouter.js";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'test') {
+	app.use(e2eRouter);
+}
+
+app.use(agendaRouter);
+app.use(errorHandlerMiddleware);
+
+export default app;
---
+++ src/routers/agendaRouter.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+//import { recommendationController } from "../controllers/recommendationController.js";
+
+const agendaRouter = Router();
+
+agendaRouter.post("/schedule",);
+agendaRouter.get("/",);
+
+export default agendaRouter;
